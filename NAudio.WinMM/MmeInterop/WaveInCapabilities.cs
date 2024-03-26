c59e73ed05e1f2725960087854c5af1c91367908
aandre-dedalord <alejandro@dedalord.com>
2024-03-26 19:43:46 -0300

    Removed code with Windows and NUnit dependencies

--- NAudio.WinMM/MmeInterop/WaveInCapabilities.cs
+++ NAudio.WinMM/MmeInterop/WaveInCapabilities.cs
@@ -113,22 +113,5 @@ namespace NAudio.Wave
         /// Default wave in guid
         /// </summary>
         public static readonly Guid DefaultWaveInGuid = new Guid("E36DC311-6D9A-11D1-A21A-00A0C9223196");
-
-        /// <summary>
-        /// The device name from the registry if supported
-        /// </summary>
-        public static string GetNameFromGuid(Guid guid)
-        {
-            // n.b it seems many audio drivers just return the default values, which won't be in the registry
-            // http://www.tech-archive.net/Archive/Development/microsoft.public.win32.programmer.mmedia/2006-08/msg00102.html
-            string name = null;
-            using (var namesKey = Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\MediaCategories"))
-            using (var nameKey = namesKey.OpenSubKey(guid.ToString("B")))
-            {
-                if (nameKey != null) name = nameKey.GetValue("Name") as string;
-            }
-            return name;
-        }
-
     }
 }
